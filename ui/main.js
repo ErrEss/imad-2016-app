2346896745d80ed429e41ebc85f365e332913d47
ErrEss <parimasa0903@gmail.com>
2016-10-11 19:39:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 console.log('Loaded!');
 
 // Change the text of the main-text div
@@ -20,4 +21,21 @@ img.onclick = function ()
 {
     var interval = setInterval(moveRight, 50);
     //img.style.marginLeft = '100px';
+};
+*/
+
+// Counter code
+
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function ()
+{
+  // Make a request to the counter endpoint
+  
+  // Capture the response and store it in a variable
+  
+  // Render a variable in the correct span
+  counter = counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
