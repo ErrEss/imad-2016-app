58f098974f7bbd2fa9e3e68c43d99aaefec649fd
ErrEss <parimasa0903@gmail.com>
2016-10-11 19:54:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,15 +27,29 @@ img.onclick = function ()
 // Counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function ()
 {
-  // Make a request to the counter endpoint
+  // Create a request object
+  var request = new XMLHttpRequest();
   
   // Capture the response and store it in a variable
+  request.onreadystatechange = function()
+  {
+    if(request.readyState === XMLHttpRequest.DONE )
+    {
+        // Take some action
+        if(request.status === 200)
+        {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+    // Not done yet
+  };
   
-  // Render a variable in the correct span
-  counter = counter+1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  // Make the request
+  request.open('GET', 'http://erress.imad.hasura-app.io/counter', true);
+  request.send(null);
+    
 };
