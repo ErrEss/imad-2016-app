c76a09c79e1febc7f39890bac61ea890ca5bf778
ErrEss <parimasa0903@gmail.com>
2016-10-12 20:07:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -68,8 +68,7 @@ button.onclick = function ()
 };
 
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
@@ -106,6 +105,8 @@ submit.onclick = function()
   };
   
   // Make the request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET', 'http://erress.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
 };
